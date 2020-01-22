ee7f382813c25965b260779eee86f38cf0b975f6
Matt Beachey <mattbeachey@gmail.com>
2020-01-21 19:13:07 -0600

    added create workout route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,25 @@ const db = require("../models")
 
 module.exports = function (app) {
 
-    // app.get()
+    app.get("/api/workouts", function (req, res) {
+        db.Workout.find({})
+            .then(function (workouts) {
+                res.json(workouts)
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    })
+
+    app.post("/api/workouts", function (req, res) {
+        db.Workout.create({})
+            .then(function (workouts) {
+                res.json(workouts)
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    })
 
 
 };
